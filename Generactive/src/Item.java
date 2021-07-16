3e4f6e54a27ccbc3ccd1422674de4c8934b3f07a
asya-khachatryan <asyakhachatryan1@gmail.com>
2021-07-16 16:25:15 +0400

    Initial commit

---
+++ Generactive/src/Item.java
@@ -0,0 +1,50 @@
+public class Item {
+    private int id;
+    private int price;
+    private String currency;
+    private String name;
+
+    public Item(int id, int price, String currency, String name) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "    * Item ID: " + id + " - " + name + " - costs " + price + " " + currency;
+    }
+}
