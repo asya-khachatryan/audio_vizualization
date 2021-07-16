88bc93850cfa1545cc9829206646498b85a67a04
asya-khachatryan <73532709+asya-khachatryan@users.noreply.github.com>
2021-07-16 16:27:35 +0400

    Delete class/src directory

--- class/src/Demo.java
+++
@@ -1,9 +0,0 @@
-public class Demo {
-    public static void main(String[] args) {
-        Engine engine = new Engine(600, 6000, 10);
-        Car car = new Car(2020, "X6", "BMW", engine);
-        System.out.println(car.getEngine().getHorsePower());
-        engine.setHorsePower(12);
-        System.out.println(car.getEngine().getHorsePower());
-    }
-}
