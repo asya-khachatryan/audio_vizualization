360c98037a2b3ab7eca84658777318b1cb6a3e39
asya-khachatryan <asyakhachatryan1@gmail.com>
2021-07-18 20:38:43 +0400

    Deleted complexity in stockitem

--- untitled/src/model/StockItem.java
+++ untitled/src/model/StockItem.java
@@ -1,13 +1,10 @@
 package model;
 
 public class StockItem extends Item {
-    private double complexity;
 
     public StockItem(int id, int price, String currency, String name,
                      Group parent, Configuration configuration) {
         super(id, price, currency, name, parent, configuration);
-        this.complexity = Math.random() + 1;
-
     }
 
     public double calculatePrice() {
