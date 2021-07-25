4d00ca65825bce3687a22c0c87e1b42684cac17b
asya-khachatryan <73532709+asya-khachatryan@users.noreply.github.com>
2021-07-25 12:30:01 +0400

    Delete untitled/src directory

--- untitled/src/model/StockItem.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class StockItem extends Item {
-
-    public StockItem(int price, String currency, String name, Group parent, Configuration configuration) {
-        super(price, currency, name, parent, configuration);
-    }
-}
